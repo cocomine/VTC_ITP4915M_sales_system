92980b51fc5c1c7f31edfa9a28b939754293587f
Ka Tung LEUNG <michaelleung6884@gmail.com>
2022-06-24 20:28:01 +0800

    4

---
+++ UI/Inventory page/Record_of_Inward_Received_Goods.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ITP4915M.Inventory_page
+{
+    partial class Record_of_Inward_Received_Goods
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Record_of_Inward_Received_Goods
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Record_of_Inward_Received_Goods";
+            this.Text = "Record_of_Inward_Received_Goods";
+            this.Load += new System.EventHandler(this.Record_of_Inward_Received_Goods_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ UI/Inventory page/Record_of_Inward_Received_Goods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITP4915M.Inventory_page
+{
+    public partial class Record_of_Inward_Received_Goods : Form
+    {
+        public Record_of_Inward_Received_Goods()
+        {
+            InitializeComponent();
+        }
+
+        private void Record_of_Inward_Received_Goods_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
