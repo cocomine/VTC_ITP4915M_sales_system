3834905b7b7f89e7d1b616893071c1171e3b6c2a
Ka Tung LEUNG <michaelleung6884@gmail.com>
2022-06-26 09:26:09 +0800

    Gv read only

--- UI/Inventory page/Item requested.Designer.cs
+++ UI/Inventory page/Item requested.Designer.cs
@@ -53,6 +53,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 35);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(776, 321);
